8de20b30fbfbcc059ff5151117d26604d5296c98
mostghoste <sarunas.jacikas@gmail.com>
2024-12-16 16:39:29 +0200

    Modify serializers

--- chat/serializers.py
+++ chat/serializers.py
@@ -5,13 +5,16 @@ class ChatRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChatRoom
         fields = ['id', 'name', 'owner', 'created_at']
+        read_only_fields = ['owner', 'created_at']  # Owner and created_at are set automatically
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = ['id', 'chatroom', 'user', 'content', 'timestamp']
+        read_only_fields = ['chatroom', 'user', 'timestamp']  # Automatically populated fields
 
 class InvitationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Invitation
         fields = ['id', 'sender', 'recipient', 'chatroom', 'status', 'sent_at']
+        read_only_fields = ['sender', 'sent_at']  # Sender and sent_at are set automatically
