c2049931b9934b40f85bc80e18278d569af42e05
mostghoste <sarunas.jacikas@gmail.com>
2024-12-16 15:21:00 +0200

    Register models to admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ChatRoom, ChatRoomMembership, Invitation, Message
 
-# Register your models here.
+admin.site.register(ChatRoom)
+admin.site.register(ChatRoomMembership)
+admin.site.register(Invitation)
+admin.site.register(Message)
